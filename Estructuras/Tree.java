66320a2a8692bf8d7eb9347c602ac487766b5123
jalector <judaalector@hotmail.com>
2018-05-20 21:26:00 -0500

    Value-Tree

--- Estructuras/Tree.java
+++ Estructuras/Tree.java
@@ -1,12 +1,12 @@
 public class Tree <Type> {
     private Node <Type> root;
-    private int size;
     private Node <Type> currentNode;
+    private int size;
 
     public Tree(Type name){
         this.root = new Node <Type> (name);
         this.size = 0;
-        currentNode = this.root;
+        this.currentNode = this.root;
     }
 
     public Node <Type> getRoot(){
@@ -48,12 +48,26 @@ public class Tree <Type> {
         return done;
     }
 
-    public Node <Type> parent(Node Type){
-        return null;
+    public Node <Type> get Parent(<Type> subtree, Type information){
+        Node <Type> aux = null,
+                    answer = null;
+            if(subtree.getInformation() == information){
+                aux = subtree;
+            }else if (subtree.hasChildren()){
+                List <Type> l = subtree.getChildren();
+                for(int i = 0, e = l.size(); i < e; i++){
+                    Node <Type> child = l.getChild(i);
+                    aux = this.getParent(child, information);
+                    if(aux != null) break;
+                }
+                if(aux != null)answer = subtree 
+            }
+        return answer;
+        
     }
 
-    public List <Type> children(Node Type){
-        return null;
+    public List <Type> children(Node <Type> node){
+        return node.getChildren();
     }
 
     public int size(){
@@ -71,22 +85,56 @@ public class Tree <Type> {
     public boolean isExternal(Node <Type> node){
         return !node.hasChildren();
     }
-
-    public void print(Node <Type> subtree, String space){
-        System.out.println(space+subtree);
+    
+    private void print(Node <Type> subtree, String space){
+        System.out.println(space+"├─"+subtree);
         if(subtree.hasChildren()){
             List <Type> l = subtree.getChildren();
             for(int i = 0, e = l.size(); i < e; i++){
-                print(l.getChild(i),space+space);
+                print(l.getChild(i),space+"│"+"  ");
+            }
+        }
+    }
+    public void print(Node <Type> subtree){
+        System.out.println(subtree);
+        if(subtree.hasChildren()){
+            List <Type> l = subtree.getChildren();
+            for(int i = 0, e = l.size(); i < e; i++){
+                print(l.getChild(i), "  ");
             }
         }
     }
 
     public static void main(String [] args){
         Tree <String> t = new Tree <String> ("Caos");
-        
+
+        t.addChildTo(t.getRoot(), "Caos", "Nix");
+        t.addChildTo(t.getRoot(), "Caos", "Érebo");
         t.addChildTo(t.getRoot(), "Caos", "Gea");
-        t.addChildTo(t.getRoot(), "Caos", "Cronos");
-        t.print(t.getRoot(), " ");
+
+        t.addChildTo(t.getRoot(), "Gea", "Temis");
+        t.addChildTo(t.getRoot(), "Gea", "Cronos");
+        t.addChildTo(t.getRoot(), "Cronos", "Hestia");
+        t.addChildTo(t.getRoot(), "Cronos", "Poseidon");
+        t.addChildTo(t.getRoot(), "Cronos", "Zeus");
+        t.addChildTo(t.getRoot(), "Zeus", "Hefesto");
+        t.addChildTo(t.getRoot(), "Zeus", "Hebe");
+        t.addChildTo(t.getRoot(), "Zeus", "Ares");
+        t.addChildTo(t.getRoot(), "Zeus", "Apolo");
+        t.addChildTo(t.getRoot(), "Zeus", "Atenea");
+        t.addChildTo(t.getRoot(), "Zeus", "Persefone");
+        t.addChildTo(t.getRoot(), "Cronos", "Hades");
+        t.addChildTo(t.getRoot(), "Cronos", "Deméter");
+        t.addChildTo(t.getRoot(), "Gea", "Rea");
+        t.addChildTo(t.getRoot(), "Gea", "Febe");
+        t.addChildTo(t.getRoot(), "Gea", "Tía");
+        t.addChildTo(t.getRoot(), "Caos", "Urano");
+        t.addChildTo(t.getRoot(), "Urano", "Erinias");
+        t.addChildTo(t.getRoot(), "Urano", "Merlina");
+        t.addChildTo(t.getRoot(), "Urano", "Afrodita");
+        t.addChildTo(t.getRoot(), "Afrodita", "Eros");
+        t.addChildTo(t.getRoot(), "Caos", "Hémera");
+        t.addChildTo(t.getRoot(), "Caos", "Éter");
+        t.print(t.getRoot());
     }
 }
---
+++ Estructuras/Value.java
@@ -0,0 +1,65 @@
+public class Value {
+    public static final int ACCESS_FRIENDLY = 0;
+    public static final int ACCESS_PUBLIC = 1;
+    public static final int ACCESS_PRIVATE = 2;
+    public static final int ACCESS_PROTECTED = 3;
+
+    public static final int TYPE_VOID = 0;
+    public static final int TYPE_STRING = 2;
+    public static final int TYPE_BYTE = 3;
+    public static final int TYPE_SHORT = 4;
+    public static final int TYPE_LONG = 5;    
+    public static final int TYPE_INT = 6;
+    public static final int TYPE_FLOAT = 7;
+    public static final int TYPE_DOUBLE = 8;
+    public static final int TYPE_OTHER = 9;
+    
+    private int accessMode;
+    private int type;
+    private String lexema;
+    private Object value;
+    private int returnValue;
+
+    public Value(int accessMode, int type, String lexema, Object value, int returnValue){
+        this.accessMode 
+    }
+
+    public void setAccessmode (int accessMode){
+        this.accessMode = accessMode;
+    }
+    public void setType (int type){
+        this.type = type;
+    }
+    public void setLexema (String lexema){
+        this.lexema = lexema;
+    }
+    public void setValue(Object value){
+        this.value = value;
+    }
+    public void setReturnValue(int returnValue){
+        this.returnValue = returnValue;
+    }
+
+    public int getaccessMode(){
+        return this.accessMode ;
+    }
+    public int getType(){
+        return this.type;
+    }
+    public String getLexema(){
+        return this.lexema;
+    }
+    public Object getValue(){
+        return this.value;
+    }
+    public int getReturnValue(){
+        return this.returnValue;
+    }
+
+    public String toString(){
+        //return "{M. accesso: "+this.accessMode+", Tipo: "+this.type+", Lexema: "+this.lex+", Value: "+this.value+", Return: "+this.returnValue+"}";
+        return "{"+this.accessMode+", "+this.type+", "+this.lex+", "+this.value+", "+this.returnValue+"}";
+    }
+
+
+}
